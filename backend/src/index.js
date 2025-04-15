e15dcf5df9fc734ede79d93711a28817712e6206
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-04-15 21:22:51 +0530

    Last commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://blabber-hub-your-hub-for-endless-chatter.vercel.app",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
