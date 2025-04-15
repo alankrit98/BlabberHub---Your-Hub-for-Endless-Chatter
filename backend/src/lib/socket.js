e335260609574ee1950e4fdc8eb4c634082c5857
ALANKRIT AGARWAL <itews2216@glbitm.ac.in>
2025-04-15 20:28:50 +0530

    Commit message

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://blabber-hub-your-hub-for-endless-chatter.vercel.app"],
+    origin: ["http://localhost:5173"],
   },
 });
 
